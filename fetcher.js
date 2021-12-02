661b79502c59df85f0e0c51242f96353576e94c9
Ren <cassandra.chen@uoit.net>
2021-12-02 02:58:38 +0000

    finally works after all these years

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,31 @@
+const net = require('net');
+const request = require('request')
 const fs = require('fs')
-let bytes = 0;
+let input = process.argv.splice(2, process.argv.length - 1)
+let localpath = input[1]
+let URL = input[0]
 
-const fetch = (url, localpatj) => {
-  process.stdin.write(`Downloaded and saved ${bytes} to ${localpath}`)
+const printMessage = (localpath) => {
+  const stats = fs.statSync(localpath).size
+  console.log(`Downloaded and saved ${stats} to ${localpath}`)
 }
 
+const fetcher = (URL, localpath) => {
+  request(URL,(err, response, body) => {
+    fs.writeFile(localpath, body, err => {
+      if (err) {
+        console.error(err)
+        console.log('statusCode:', response && response.statusCode); 
+        return
+      }
+      printMessage(localpath)
+    })
+  })
+};
+
+fetcher(URL, localpath)
+
+//Remember for later
 /*
 200: "Everything went great!"
 201: "The request has succeeded and a new resource has been created as a result."
